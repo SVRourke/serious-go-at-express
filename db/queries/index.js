ca83362580b4af114af9ab7ca70d4bcf533a89f8
Samuel Rourke <samuelvanrourke@gmail.com>
2021-10-03 15:03:09 -0400

    added newUser query

--- db/queries/index.js
+++ db/queries/index.js
@@ -1,9 +1,10 @@
 const { usersTable, listsTable, todosTable } = require("./tables");
-
+const { newUser } = require("./users");
 module.exports = {
   // "Migrations"
   usersTable,
   listsTable,
   todosTable,
   // SOMETHING ELSE
+  newUser,
 };
