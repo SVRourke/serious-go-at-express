eb74010784eedafd1c2e08883781538bef3f0639
Samuel Rourke <samuelvanrourke@gmail.com>
2021-10-03 22:56:08 -0400

    organized exports added item and list queries

--- db/queries/index.js
+++ db/queries/index.js
@@ -1,10 +1,35 @@
 const { usersTable, listsTable, todosTable } = require("./tables");
+
 const { newUser } = require("./users");
+
+const { newList, indexList, deleteList } = require("./lists");
+
+const {
+  indexItem,
+  createItem,
+  updateItem,
+  toggleItem,
+  destroyItem,
+} = require("./items");
+
 module.exports = {
   // "Migrations"
   usersTable,
   listsTable,
   todosTable,
-  // SOMETHING ELSE
+
+  // Users
   newUser,
+
+  // Lists
+  newList,
+  indexList,
+  deleteList,
+
+  // Items
+  indexItem,
+  createItem,
+  updateItem,
+  toggleItem,
+  destroyItem,
 };
