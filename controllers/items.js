e5c1a0e456468d9d24f254b261b21d67916415b1
SVRourke <samuelvanrourke@gmail.com>
2021-10-04 16:56:35 -0400

    added update and destroy action logic, todos

--- controllers/items.js
+++ controllers/items.js
@@ -22,6 +22,7 @@ exports.index = async (req, res) => {
   }
 };
 
+// TODO: Create View
 exports.new = (req, res) => {
   res.send("new Item form route");
 };
@@ -38,15 +39,38 @@ exports.create = async (req, res) => {
     res.send(error);
   }
 };
+
+// TODO Create Edit Form
 exports.edit = async (req, res) => {
+  // GET Item
+  // DISPLAY View With Item info as variables
   res.send("Item edit form route");
 };
+
 exports.update = async (req, res) => {
-  res.send("Item update route");
+  try {
+    const updated = await pool.query(updateItem, [
+      req.params.item_id,
+      req.body.text,
+    ]);
+    res.send(updated.rows[0]);
+  } catch (error) {
+    res.send(error);
+  }
 };
+
 exports.toggle = async (req, res) => {
+  // GET ITEM
+  // UPDATE ITEM
+  // RETURN ITEM
   res.send("Item toggle route");
 };
+
 exports.destroy = async (req, res) => {
-  res.send("Item delete route");
+  try {
+    const deletion = await pool.query(destroyItem, [params.item_id]);
+    res.status(200);
+  } catch (error) {
+    res.send(error);
+  }
 };
